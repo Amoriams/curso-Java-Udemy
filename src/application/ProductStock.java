f8ca9ff9d0bcdc4ca83c54873ed950bd48a204a5
hellammmort <julia.amorim.599@gmail.com>
2025-07-15 18:34:23 -0300

    atualizando o exercicio do curso

--- src/application/ProductStock.java
+++ src/application/ProductStock.java
@@ -10,11 +10,14 @@ public class ProductStock {
         Scanner sc = new Scanner(System.in);
 
         ProductN product = new ProductN();
+
         System.out.println("Enter product data");
         System.out.print("name: ");
         product.name = sc.nextLine();
+
         System.out.print("price: ");
         product.price = sc.nextDouble();
+
         System.out.print("quantity in stock: ");
         product.quantity = sc.nextInt();
 
@@ -22,14 +25,20 @@ public class ProductStock {
         System.out.println("product data: " + product);
 
         System.out.println();
-        System.out.println("Enter the number of products to be added in stock: ");
+        System.out.print("Enter the number of products to be added in stock: ");
         int quantity = sc.nextInt();
-        
+        product.addProduct(quantity);
 
+        System.out.println();
+        System.out.println("updated data: " + product);
 
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
 
-
-
+        System.out.println();
+        System.out.println("updated data: " + product);
 
 
         sc.close();
